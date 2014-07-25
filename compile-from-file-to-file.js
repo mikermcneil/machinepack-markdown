35086d824ecc12a9e40802dfc9c0593193f3e68f
Mike McNeil <mike@balderdash.co>
2014-07-25 03:59:32 -0500

    Use fs-extra's 'outputFile' in order to do a mkdir -p and avoid ENOENTs

--- compile-from-file-to-file.js
+++ compile-from-file-to-file.js
@@ -2,7 +2,7 @@
  *
  * ----------------------------------------
  * Usage:
- * 
+ *
  * require('node-machine')
  * .load('./lib/compile-from-file-to-file')
  * .configure(...)
@@ -17,28 +17,28 @@ module.exports = {
   //
   // Steps:
   // ======
-  // 
+  //
   // • stream bytes from disk
-  // 
+  //
   // • when all bytes are in RAM, call the `beforeConvert()` lifecycle
   //   hook (if one exists) to perform an optional transformation of the
   //   markdown string.
-  // 
+  //
   // • convert the (possibly now-transformed) markdown to HTML
-  // 
+  //
   // • call the `afterConvert()` lifecycle hook (if one exists) to perform
   //   an optional transformation of the HTML string.
-  //   
+  //
   // • send the bytes of the (possibly now-transformed) HTML to the path on
   //   disk specified by `dest.html` as a write stream.  When the stream
   //   finishes, call the async callback to signal that this markdown file
   //   has been compiled to HTML and written to disk successfully.
-  // 
+  //
   // • if any error occurs, bail out of trying to compile/write this particular
   //   template file and push it to an error stack for this build step which is
   //   available in closure scope.  It will be handled later, but shouldn't prevent
   //   the other template files from being compiled/written.
-  // 
+  //
   dependencies: {
     'node-machine': '*',
     'fs-extra': '*'
@@ -89,7 +89,7 @@ module.exports = {
           $i.afterConvert(htmlString, function(err, htmlString) {
             if (err) return $x.couldNotCompile(err);
 
-            fsx.writeFile($i.dest, htmlString, function(err) {
+            fsx.outputFile($i.dest, htmlString, function(err) {
               if (err) return $x.couldNotWrite(err);
               return $x.success();
             });
